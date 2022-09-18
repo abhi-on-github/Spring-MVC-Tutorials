b7714934816e6b27dcb3f72fbe96e65c9b2743a3
Abhishek Jain <abhi85ster@gmail.com>
2022-09-17 23:20:34 -0700

    Dynamo DB Spring Boot Practice

---
+++ exercise/DynamoDB-SpringBoot-Demo/src/main/java/com/practice/DynamoDBSpringBootDemo/config/DynamoDBConfiguration.java
@@ -0,0 +1,40 @@
+package com.practice.DynamoDBSpringBootDemo.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfiguration {
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "dynamodb.us-west-1.amazonaws.com",
+                                "us-west-1"
+                        )
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(
+                                        "AKIAXGOYIISYVXK3CGJS",
+                                        "38e/Je6o7D0qMpSxb/aSzVEE1bQR2fkxi/lLmike"
+                                )
+                        )
+                )
+                .build();
+    }
+
+}
